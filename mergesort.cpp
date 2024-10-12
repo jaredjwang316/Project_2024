b616a9d05aa3d50fc1273849044c161f1914b047
jaredjwang316 <jwangj001@tamu.edu>
2024-10-12 11:52:36 -0500

    Modified the merge sort caliper areas to meet the requirements of the markdown file

--- mergesort.cpp
+++ mergesort.cpp
@@ -52,12 +52,21 @@ std::vector<int> merge_sort(std::vector<int> arr) {
     return merge(left, right);
 }
 
+// Function to check if the array is sorted
+bool is_sorted(const std::vector<int>& arr) {
+    for (size_t i = 1; i < arr.size(); i++) {
+        if (arr[i] < arr[i - 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
 int main(int argc, char *argv[]) {
     CALI_CXX_MARK_FUNCTION;
 
     const int MASTER = 0;
-    const int FROM_MASTER = 1;
-    const int FROM_WORKER = 2;
 
     int sizeOfArray;
     if (argc == 2) {
@@ -67,262 +76,167 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
-    int numtasks, taskid, numworkers, source, dest, mtype;
-    int averow, extra, offset, i, rc;
+    int numtasks, taskid;
     std::vector<int> array;
     std::vector<int> subarray;
-    std::vector<int> sorted_array;
-    MPI_Status status;
-    double worker_receive_time, worker_calculation_time, worker_send_time = 0;
-    double whole_computation_time, master_initialization_time, master_send_receive_time = 0;
-    const char* whole_computation = "whole_computation";
-    const char* master_initialization = "master_initialization";
-    const char* master_send_receive = "master_send_recieve";
-    const char* worker_receive = "worker_recieve";
-    const char* worker_calculation = "worker_calculation";
-    const char* worker_send = "worker_send";
 
-    
     // Initialize MPI
+    CALI_MARK_BEGIN("MPI_Init");
+    
     MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &taskid);
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
-
-    if (numtasks < 2) {
-        std::cout << "Need at least two MPI tasks. Quitting..." << std::endl;
-        MPI_Abort(MPI_COMM_WORLD, rc);
-        exit(1);
-    }
-
-    numworkers = numtasks - 1;
-
-    MPI_Comm workers_comm;
-    int color;  // Defines the color for the MPI_Comm_split
-    int key;    // Defines the rank order within the new communicator
+    MPI_Comm_rank(MPI_COMM_WORLD, &taskid);
     
-    if (taskid == MASTER) {
-        color = MPI_UNDEFINED;  // Exclude the master from the new communicator
-    } else {
-        color = 1;  // All workers will have the same color
-    }
-    key = taskid;  // Use task ID to define the rank ordering within the new communicator
-    
-    // Create a new communicator with the workers
-    MPI_Comm_split(MPI_COMM_WORLD, color, key, &workers_comm);
+    CALI_MARK_END("MPI_Init");  // End MPI_Init
 
-    // Timing and profiling initialization
-    double whole_computation_start = MPI_Wtime();
-    CALI_MARK_BEGIN(whole_computation);
+    // Start Adiak and register metadata
+    adiak::init(nullptr);
+    adiak::launchdate();    // launch date of the job
+    adiak::libraries();     // Libraries used
+    adiak::cmdline();  // Command line used to launch the job
+    adiak::clustername();   // Name of the cluster
 
-    // Create caliper ConfigManager object
-    cali::ConfigManager mgr;
-    mgr.start(); // Start profiling
+    // Algorithm-specific metadata
+    std::string algorithm = "merge";
+    std::string programming_model = "mpi";
+    std::string data_type = "int";
+    int size_of_data_type = sizeof(int);
+    std::string input_type = "Random";
+    std::string scalability = "strong";  
+    int group_number = 5;  
+    std::string implementation_source = "handwritten";  
 
-    /**************************** master task ************************************/
-    if (taskid == MASTER) {
-        double master_init_start = MPI_Wtime();
-        CALI_MARK_BEGIN(master_initialization);
-
-        // Allocate and initialize the array
-        array.resize(sizeOfArray);
-        for (i = 0; i < sizeOfArray; i++) {
-            array[i] = rand() % 100; // Initialize with random values
-        }
-
-        double master_init_end = MPI_Wtime();
-        master_initialization_time = master_init_end - master_init_start;
-        CALI_MARK_END(master_initialization);
-
-        double master_send_receive_start = MPI_Wtime();
-        CALI_MARK_BEGIN(master_send_receive);
-
-        // Distribute array segments to worker tasks
-        averow = sizeOfArray / numworkers;
-        extra = sizeOfArray % numworkers;
-        offset = 0;
-        mtype = FROM_MASTER;
-
-        for (dest = 1; dest <= numworkers; dest++) {
-            int elements = (dest <= extra) ? averow + 1 : averow;
-            MPI_Send(&elements, 1, MPI_INT, dest, mtype, MPI_COMM_WORLD);
-            MPI_Send(array.data() + offset, elements, MPI_INT, dest, mtype, MPI_COMM_WORLD);
-            offset += elements;
-        }
-
-        // Collect sorted segments from worker tasks
-        sorted_array.resize(sizeOfArray);
-        offset = 0;
-        mtype = FROM_WORKER;
-        for (i = 1; i <= numworkers; i++) {
-            int elements;
-            MPI_Recv(&elements, 1, MPI_INT, i, mtype, MPI_COMM_WORLD, &status);
-            MPI_Recv(sorted_array.data() + offset, elements, MPI_INT, i, mtype, MPI_COMM_WORLD, &status);
-            offset += elements;
-        }
-
-        double master_send_receive_end = MPI_Wtime();
-        master_send_receive_time = master_send_receive_end - master_send_receive_start;
-        CALI_MARK_END(master_send_receive);
-
-        // Merge the sorted segments
-        sorted_array = merge_sort(sorted_array);  // Use the correct merge_sort call here
-
-        // Optionally print the sorted array
-        /*
-        std::cout << "Sorted Array:" << std::endl;
-        for (i = 0; i < sizeOfArray; i++) {
-            std::cout << sorted_array[i] << " ";
-        }
-        std::cout << std::endl;
-        */
-
-    }
-
-    /**************************** worker task ************************************/
-    if (taskid > MASTER) {
-        double worker_receive_start = MPI_Wtime();
-        CALI_MARK_BEGIN(worker_receive);
-
-        int elements;
-        mtype = FROM_MASTER;
-        MPI_Recv(&elements, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD, &status);
-
-        subarray.resize(elements);
-        MPI_Recv(subarray.data(), elements, MPI_INT, MASTER, mtype, MPI_COMM_WORLD, &status);
-
-        double worker_receive_end = MPI_Wtime();
-        worker_receive_time = worker_receive_end - worker_receive_start;
-        CALI_MARK_END(worker_receive);
-
-        double worker_calculation_start = MPI_Wtime();
-        CALI_MARK_BEGIN(worker_calculation);
-
-        // Sort the subarray using merge sort
-        subarray = merge_sort(subarray);  
-
-        double worker_calculation_end = MPI_Wtime();
-        worker_calculation_time = worker_calculation_end - worker_calculation_start;
-        CALI_MARK_END(worker_calculation);
-
-        double worker_send_start = MPI_Wtime();
-        CALI_MARK_BEGIN(worker_send);
-
-        mtype = FROM_WORKER;
-        MPI_Send(&elements, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD);
-        MPI_Send(subarray.data(), elements, MPI_INT, MASTER, mtype, MPI_COMM_WORLD);
-
-        double worker_send_end = MPI_Wtime();
-        worker_send_time = worker_send_end - worker_send_start;
-        CALI_MARK_END(worker_send);
-
-    }
-
-    double whole_computation_end = MPI_Wtime();
-    whole_computation_time = whole_computation_end - whole_computation_start;
-    CALI_MARK_END(whole_computation);
-
-
-    double worker_receive_time_max, worker_receive_time_min, worker_receive_time_sum, worker_receive_time_average;
-    double worker_calculation_time_max, worker_calculation_time_min, worker_calculation_time_sum, worker_calculation_time_average;
-    double worker_send_time_max, worker_send_time_min, worker_send_time_sum, worker_send_time_average;
-
-    // Initialize Adiak
-    adiak::init(NULL);
-    adiak::user();
-    adiak::launchdate();
-    adiak::libraries();
-    adiak::cmdline();
-    adiak::clustername();
+    adiak::value("algorithm", algorithm);
+    adiak::value("programming_model", programming_model);
+    adiak::value("data_type", data_type);
+    adiak::value("size_of_data_type", size_of_data_type);
+    adiak::value("input_size", sizeOfArray);
+    adiak::value("input_type", input_type);
     adiak::value("num_procs", numtasks);
-    adiak::value("program_name", "mpi_merge_sort");
+    adiak::value("scalability", scalability);
+    adiak::value("group_num", group_number);
+    adiak::value("implementation_source", implementation_source);
 
 
-    // Use MPI_Reduce to calculate statistics for worker processes
-    if (workers_comm != MPI_COMM_NULL) {
-        MPI_Reduce(&worker_receive_time, &worker_receive_time_min, 1, MPI_DOUBLE, MPI_MIN, 0, workers_comm);
-        MPI_Reduce(&worker_receive_time, &worker_receive_time_max, 1, MPI_DOUBLE, MPI_MAX, 0, workers_comm);
-        MPI_Reduce(&worker_receive_time, &worker_receive_time_sum, 1, MPI_DOUBLE, MPI_SUM, 0, workers_comm);
+    CALI_MARK_BEGIN("MPI_Comm_dup");  // Begin MPI_Comm_dup
+    MPI_Comm comm_dup;
+    MPI_Comm_dup(MPI_COMM_WORLD, &comm_dup);
+    CALI_MARK_END("MPI_Comm_dup");  // End MPI_Comm_dup
+
+    // Calculate chunk size and extra elements
+    int chunk_size = sizeOfArray / numtasks;
+    int remainder = sizeOfArray % numtasks;
+
+    // Resize subarray to receive its portion of data
+    int subarray_size;
+    
+    if (taskid < remainder)
+    {
+      subarray_size = chunk_size + 1;
+    }
+    else
+    {
+      subarray_size = chunk_size;
+    }
+    
+    subarray.resize(subarray_size);
+
+    // Data initialization runtime (assuming data initialization code here)
+    CALI_MARK_BEGIN("data_init_runtime");
+
+    if (taskid == MASTER) {
+        array.resize(sizeOfArray);
+        for (int i = 0; i < sizeOfArray; i++) {
+            array[i] = rand() % 1000;  // Random initialization of array
+        }
+    }
+    CALI_MARK_END("data_init_runtime");  // End data_init_runtime
+
+    // Barrier to synchronize
+    CALI_MARK_BEGIN("comm");
+    MPI_Barrier(MPI_COMM_WORLD);
+    CALI_MARK_END("comm");
+
+    // Prepare data for scattering: send counts and displacements
+    std::vector<int> send_counts(numtasks);
+    std::vector<int> displacements(numtasks);
+    int offset = 0;
+
+    for (int i = 0; i < numtasks; i++) {
+        if (i < remainder)
+        {
+          send_counts[i] = chunk_size + 1;
+        }
+        else    
+        {
+          send_counts[i] = chunk_size;
+        }
         
-        MPI_Reduce(&worker_send_time, &worker_send_time_min, 1, MPI_DOUBLE, MPI_MIN, 0, workers_comm);
-        MPI_Reduce(&worker_send_time, &worker_send_time_max, 1, MPI_DOUBLE, MPI_MAX, 0, workers_comm);
-        MPI_Reduce(&worker_send_time, &worker_send_time_sum, 1, MPI_DOUBLE, MPI_SUM, 0, workers_comm);
-        
-        MPI_Reduce(&worker_calculation_time, &worker_calculation_time_min, 1, MPI_DOUBLE, MPI_MIN, 0, workers_comm);
-        MPI_Reduce(&worker_calculation_time, &worker_calculation_time_max, 1, MPI_DOUBLE, MPI_MAX, 0, workers_comm);
-        MPI_Reduce(&worker_calculation_time, &worker_calculation_time_sum, 1, MPI_DOUBLE, MPI_SUM, 0, workers_comm);     
+        displacements[i] = offset;
+        offset += send_counts[i];
     }
 
-    if (taskid == 0) {
-        // Master process reports timing information
-        printf("******************************************************\n");
-        printf("Master Times:\n");
-        printf("Whole Computation Time: %f \n", whole_computation_time);
-        printf("Master Initialization Time: %f \n", master_initialization_time);
-        printf("Master Send and Receive Time: %f \n", master_send_receive_time);
-        printf("\n******************************************************\n");
+    // Scatter data to workers with varying counts
+    CALI_MARK_BEGIN("comm_large");
+    MPI_Scatterv(array.data(), send_counts.data(), displacements.data(), MPI_INT, subarray.data(), subarray.size(), MPI_INT, MASTER, MPI_COMM_WORLD);
+    CALI_MARK_END("comm_large");
 
-        // Add values to Adiak
-        adiak::value("MPI_Reduce-whole_computation_time", whole_computation_time);
-        adiak::value("MPI_Reduce-master_initialization_time", master_initialization_time);
-        adiak::value("MPI_Reduce-master_send_receive_time", master_send_receive_time);
+    // Computation section
+    CALI_MARK_BEGIN("comp");
+    subarray = merge_sort(subarray);
+    CALI_MARK_END("comp");
 
-        // Receive statistics from the first worker
-        MPI_Recv(&worker_receive_time_max, 1, MPI_DOUBLE, 1, FROM_WORKER, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Recv(&worker_receive_time_min, 1, MPI_DOUBLE, 1, FROM_WORKER, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Recv(&worker_receive_time_average, 1, MPI_DOUBLE, 1, FROM_WORKER, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Recv(&worker_calculation_time_max, 1, MPI_DOUBLE, 1, FROM_WORKER, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Recv(&worker_calculation_time_min, 1, MPI_DOUBLE, 1, FROM_WORKER, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Recv(&worker_calculation_time_average, 1, MPI_DOUBLE, 1, FROM_WORKER, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Recv(&worker_send_time_max, 1, MPI_DOUBLE, 1, FROM_WORKER, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Recv(&worker_send_time_min, 1, MPI_DOUBLE, 1, FROM_WORKER, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Recv(&worker_send_time_average, 1, MPI_DOUBLE, 1, FROM_WORKER, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
-        // Log worker statistics
-        adiak::value("MPI_Reduce-worker_receive_time_max", worker_receive_time_max);
-        adiak::value("MPI_Reduce-worker_receive_time_min", worker_receive_time_min);
-        adiak::value("MPI_Reduce-worker_receive_time_average", worker_receive_time_average);
-        adiak::value("MPI_Reduce-worker_calculation_time_max", worker_calculation_time_max);
-        adiak::value("MPI_Reduce-worker_calculation_time_min", worker_calculation_time_min);
-        adiak::value("MPI_Reduce-worker_calculation_time_average", worker_calculation_time_average);
-        adiak::value("MPI_Reduce-worker_send_time_max", worker_send_time_max);
-        adiak::value("MPI_Reduce-worker_send_time_min", worker_send_time_min);
-        adiak::value("MPI_Reduce-worker_send_time_average", worker_send_time_average);
-    } else if (taskid == 1) {
-        // Worker process calculates and sends statistics to the master
-        worker_receive_time_average = worker_receive_time_sum / (double)numworkers;
-        worker_calculation_time_average = worker_calculation_time_sum / (double)numworkers;
-        worker_send_time_average = worker_send_time_sum / (double)numworkers;
-
-        printf("******************************************************\n");
-        printf("Worker Times:\n");
-        printf("Worker Receive Time Max: %f \n", worker_receive_time_max);
-        printf("Worker Receive Time Min: %f \n", worker_receive_time_min);
-        printf("Worker Receive Time Average: %f \n", worker_receive_time_average);
-        printf("Worker Calculation Time Max: %f \n", worker_calculation_time_max);
-        printf("Worker Calculation Time Min: %f \n", worker_calculation_time_min);
-        printf("Worker Calculation Time Average: %f \n", worker_calculation_time_average);
-        printf("Worker Send Time Max: %f \n", worker_send_time_max);
-        printf("Worker Send Time Min: %f \n", worker_send_time_min);
-        printf("Worker Send Time Average: %f \n", worker_send_time_average);
-        printf("\n******************************************************\n");
-
-        // Send the computed statistics back to the master
-        MPI_Send(&worker_receive_time_max, 1, MPI_DOUBLE, MASTER, FROM_WORKER, MPI_COMM_WORLD);
-        MPI_Send(&worker_receive_time_min, 1, MPI_DOUBLE, MASTER, FROM_WORKER, MPI_COMM_WORLD);
-        MPI_Send(&worker_receive_time_average, 1, MPI_DOUBLE, MASTER, FROM_WORKER, MPI_COMM_WORLD);
-        MPI_Send(&worker_calculation_time_max, 1, MPI_DOUBLE, MASTER, FROM_WORKER, MPI_COMM_WORLD);
-        MPI_Send(&worker_calculation_time_min, 1, MPI_DOUBLE, MASTER, FROM_WORKER, MPI_COMM_WORLD);
-        MPI_Send(&worker_calculation_time_average, 1, MPI_DOUBLE, MASTER, FROM_WORKER, MPI_COMM_WORLD);
-        MPI_Send(&worker_send_time_max, 1, MPI_DOUBLE, MASTER, FROM_WORKER, MPI_COMM_WORLD);
-        MPI_Send(&worker_send_time_min, 1, MPI_DOUBLE, MASTER, FROM_WORKER, MPI_COMM_WORLD);
-        MPI_Send(&worker_send_time_average, 1, MPI_DOUBLE, MASTER, FROM_WORKER, MPI_COMM_WORLD);
+    // Prepare receive counts and displacements for gathering
+    std::vector<int> recv_counts(numtasks);
+    std::vector<int> recv_displacements(numtasks);
+    offset = 0;
+    for (int i = 0; i < numtasks; i++) {
+        recv_counts[i] = send_counts[i]; // Same as the send counts
+        recv_displacements[i] = displacements[i];
+    }
+    
+    // Resize the gathered array on the MASTER process before gathering data
+    if (taskid == MASTER) {
+        array.resize(sizeOfArray);
     }
 
-    // Flush Caliper output before finalizing MPI
-    mgr.stop();
-    mgr.flush();
+    // Gather sorted data back to master
+    CALI_MARK_BEGIN("comp_large");
+    MPI_Gatherv(subarray.data(), subarray.size(), MPI_INT, array.data(), recv_counts.data(), recv_displacements.data(), MPI_INT, MASTER, MPI_COMM_WORLD);
+    CALI_MARK_END("comp_large");
 
-    // Finalize MPI
+    // Check correctness
+    CALI_MARK_BEGIN("correctness_check");
+    if (taskid == MASTER) {
+        // The master process needs to merge the sorted subarrays into a single sorted array
+        std::vector<int> sorted_array;
+
+        // Merge the sorted subarrays back into the final sorted array
+        for (int i = 0; i < numtasks; ++i) {
+            // Extract the subarray that belongs to this task
+            std::vector<int> current_subarray(array.begin() + recv_displacements[i], 
+                                               array.begin() + recv_displacements[i] + recv_counts[i]);
+            
+            // Merge current subarray into the sorted_array
+            sorted_array = merge(sorted_array, current_subarray);
+        }
+
+        // Now sorted_array contains the fully sorted array
+        array = sorted_array;
+        
+        if (is_sorted(array)) {
+            std::cout << "Array is sorted correctly." << std::endl;
+        } else {
+            std::cout << "Array is NOT sorted correctly." << std::endl;
+        }
+    }
+    CALI_MARK_END("correctness_check");
+
+    // Finalize MPI and Adiak
+    CALI_MARK_BEGIN("MPI_Finalize");
+    adiak::fini();
     MPI_Finalize();
+    CALI_MARK_END("MPI_Finalize");
+
     return 0;
 }
